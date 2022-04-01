351854341d86308c5ebe243bf9aecffc9a180f67
Juan Camilo <juanktronica@hotmail.com>
2022-03-31 22:00:10 -0500

    apply format

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -24,7 +24,7 @@ export class UsersService {
   ];
 
   findAll() {
-    const apiKey =  this.configService.get('API_KEY');
+    const apiKey = this.configService.get('API_KEY');
     const dbName = this.configService.get('DATABASE_NAME');
     console.log(apiKey, dbName);
     return this.users;
